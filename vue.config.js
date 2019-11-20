d188fac74972b3b6633cb0c617bbaa82d58ac464
daiqiuyue <986799852@qq.com>
2019-11-20 21:10:24 +0800

    更新了vue.config.json

--- vue.config.js
+++ vue.config.js
@@ -5,25 +5,5 @@ module.exports = {
 
   // 关闭ESLint编译
   lintOnSave: false,
-
-  configureWebpack : {
-    resolve: {
-      extensions: ['.js', '.vue', '.json'],
-      alias: {
-        'vue$': 'vue/dist/vue.esm.js',
-        '@': resolve('src'),
-        'components': resolve('src/components'),
-      }
-    },
-  },
-
-  css: { // 添加postcss配置
-    loaderOptions: {
-      postcss: {
-        plugins: [
-          postcss
-        ]
-      }
-    }
-  },
+ 
 } 
