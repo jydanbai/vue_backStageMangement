a7949b68f799bbc217b521b2b0b089a294998234
qiaoquan <1486314790@qq.com>
2019-11-21 23:18:26 +0800

    路由写错-已修改

--- src/pages/Products/Products.vue
+++ src/pages/Products/Products.vue
@@ -1,62 +1,7 @@
 <template>
   <div>
     <router-view></router-view>
-    <el-card shadow="never" class="yqq-tianjiashangpin"  >
-      <!-- 步骤条 -->
-      <el-steps :active="2" align-center class="yqq-jindutiao"   >
-        <el-step  description="填写商品信息"></el-step>
-        <el-step  description="填写商品信息"></el-step>
-        <el-step  description="填写商品信息"></el-step>
-        <el-step  description="填写商品信息"></el-step>
-      </el-steps>
-      <!--添加商品  -->
-      <el-form  label-width="80px">
-        <el-form-item label="* 商品分类">
-          <el-select  placeholder="请选择">
-            <el-option label="苹果" ></el-option>
-            <el-option label="西瓜" ></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="* 商品名称">
-          <el-input    ></el-input>
-        </el-form-item>
-        <el-form-item label="* 副标题">
-          <el-input    ></el-input>
-        </el-form-item>
-        <el-form-item label="* 商品品牌">
-          <el-select  placeholder="请选择">
-            <el-option label="苹果" "></el-option>
-            <el-option label="西瓜" "></el-option>
-          </el-select>
-        </el-form-item>
-         <el-form-item label="商品介绍">
-          <el-input type="textarea" v-model="form.desc"></el-input>
-        </el-form-item>
-        <el-form-item label="商品货号">
-          <el-input    ></el-input>
-        </el-form-item>
-        <el-form-item label="商品售价">
-          <el-input    ></el-input>
-        </el-form-item>
-        <el-form-item label="市场价">
-          <el-input    ></el-input>
-        </el-form-item>
-        <el-form-item label="商品库存">
-          <el-input    ></el-input>
-        </el-form-item>
-        <el-form-item label="计量单位">
-          <el-input    ></el-input>
-        </el-form-item>
-        <el-form-item label="商品重量">
-          <el-input    ></el-input>
-        </el-form-item>
-        <el-form-item label="排序">
-          <el-input    ></el-input>
-        </el-form-item   >
-          
-      </el-form>
-      <el-button type="primary" class="yqq-xiayibu" >下一步填写商品促销</el-button>
-    </el-card>
+
   </div>
 </template>
 
