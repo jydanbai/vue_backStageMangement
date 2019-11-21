132e86fcce48c501bb8686f4646d539d403a7e31
Jy <18222595056@163.com>
2019-11-21 20:18:33 +0800

    用户页面初始化

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
 <template>
   <div id="app">
     <Category v-if="$route.meta.isShowCategory" />
-   <div class="rightContent">
+    <div class="rightContent">
       <Header v-if="$route.meta.isShowCategory" />
-    <router-view></router-view>
-   </div>
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
-  import Category from './components/Category/Category'
-  import Header from './components/Header/Header'
+import Category from "./components/Category/Category";
+import Header from "./components/Header/Header";
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Category,
     Header
   }
-}
+};
 </script>
 
 <style lang='stylus' rel='stylesheet/stylus' scoped>
-  #app
-    display flex
-  .rightContent
-    width 100%
+#app {
+  display: flex;
+}
+
+.rightContent {
+  width: 100%;
+}
 </style>
 
 
