6659342b9129ee78e54d252d953b9ea20d228d67
qiaoquan <1486314790@qq.com>
2019-11-21 10:36:31 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
     <h2>hello</h2>
+    <Products/>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import Products from "./pages/Products/Products";
   export default {
+    components:{
+      Products
+    }
   }
 </script>
 
