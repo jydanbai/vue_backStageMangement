5de334cbab397fc0617c0e86206597f693094d04
Jy <18222595056@163.com>
2019-11-21 13:41:15 +0800

    1

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import App from './App.vue'
 
 import router from './router'
 import './plugins/element.js'
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
 
+Vue.use(ElementUI)
+Vue.use(Vuex)
 // 注册全局组件
 
 // Vue.component()
