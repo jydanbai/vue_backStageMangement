46765db9c1244e4ac95d9a0a6db92e4abf26a695
daiqiuyue <986799852@qq.com>
2019-11-21 11:48:00 +0800

    侧边导航更新

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <template>
-  <div>
+  <div id="app">
     <Category/>
-    <router-view></router-view>
   </div>
 </template>
+
 <script>
-
- import Category from './components/Category/Category'
-  export default {
-    components:{
-      Category
-    }
+  import Category from './components/Category/Category'
+export default {
+  name: 'app',
+  components: {
+    Category
   }
+}
 </script>
-<style lang="stylus" rel="stylesheet/stylus">
 
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
--- src/components/Category/Category.vue
+++ src/components/Category/Category.vue
@@ -1,66 +1,84 @@
 <template>
-  <div>
-    <el-row class="tac">
-      <el-col :span="12">
-        <h5>自定义颜色</h5>
-        <el-menu
-          default-active="2"
-          class="el-menu-vertical-demo"
-          @open="handleOpen"
-          @close="handleClose"
-          background-color="#545c64"
-          text-color="#fff"
-          active-text-color="#ffd04b"
-        >
-          <el-submenu index="1">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>导航一</span>
-            </template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="1-1">选项1</el-menu-item>
-              <el-menu-item index="1-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="1-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="1-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="1-4-1">选项1</el-menu-item>
-            </el-submenu>
-          </el-submenu>
-          <el-menu-item index="2">
-            <i class="el-icon-menu"></i>
-            <span slot="title">导航二</span>
-          </el-menu-item>
-          <el-menu-item index="3" disabled>
-            <i class="el-icon-document"></i>
-            <span slot="title">导航三</span>
-          </el-menu-item>
-          <el-menu-item index="4">
-            <i class="el-icon-setting"></i>
-            <span slot="title">导航四</span>
-          </el-menu-item>
-        </el-menu>
-      </el-col>
-    </el-row>
+  <div id="categoryContainer">
+  <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;" class="btns">
+  <el-radio-button :label="false">展开</el-radio-button>
+  <el-radio-button :label="true">收起</el-radio-button>
+</el-radio-group>
+<el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
+  <el-submenu index="1">
+    <template slot="title">
+      <i class="el-icon-location"></i>
+      <span slot="title">首页</span>
+    </template>
+    </el-submenu>
+     <el-submenu index="1">
+    <template slot="title">
+      <i class="el-icon-menu"></i>
+      <span slot="title">商品</span>
+    </template>
+    <el-menu-item-group>
+      <el-menu-item index="1-1">选项1</el-menu-item>
+      <el-menu-item index="1-2">选项2</el-menu-item>
+      <el-menu-item index="1-3">选项3</el-menu-item>
+      <el-menu-item index="1-3">选项4</el-menu-item>
+    </el-menu-item-group>
+    </el-submenu>
+     <el-submenu index="1">
+    <template slot="title">
+      <i class="el-icon-document"></i>
+      <span slot="title">订单</span>
+    </template>
+    <el-menu-item-group>
+      <el-menu-item index="1-1">选项1</el-menu-item>
+      <el-menu-item index="1-2">选项2</el-menu-item>
+      <el-menu-item index="1-3">选项3</el-menu-item>
+      <el-menu-item index="1-3">选项4</el-menu-item>
+    </el-menu-item-group>
+    </el-submenu>
+     <el-submenu index="1">
+    <template slot="title">
+         <i class="el-icon-setting"></i>
+      <span slot="title">权限</span>
+    </template>
+    <el-menu-item-group>
+      <el-menu-item index="1-1">选项1</el-menu-item>
+      <el-menu-item index="1-2">选项2</el-menu-item>
+      <el-menu-item index="1-3">选项3</el-menu-item>
+      <el-menu-item index="1-3">选项4</el-menu-item>
+    </el-menu-item-group>
+    </el-submenu>
+</el-menu>
   </div>
 </template>
 
 <script>
-export default {
-  methods: {
-    handleOpen(key, keyPath) {
-      console.log(key, keyPath);
+  export default {
+    data() {
+      return {
+        isCollapse: true
+      };
     },
-    handleClose(key, keyPath) {
-      console.log(key, keyPath);
+    methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
     }
   }
-};
 </script>
 
-<style>
-
+<style lang='stylus' rel='stylesheet/stylus' >
+  #categoryContainer
+    .el-menu-vertical-demo:not(.el-menu--collapse)
+      position relative
+      width 200px
+      height 100%
+    .btns
+      position absolute
+      left 20px
+      top 10px
 </style>
+
+
