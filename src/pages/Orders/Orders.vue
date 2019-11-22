3ac8bc1afe4b4d9b4b88dcd99b29cf46607fc9c7
qiaoquan <1486314790@qq.com>
2019-11-22 19:26:02 +0800

    11

--- src/pages/Orders/Orders.vue
+++ src/pages/Orders/Orders.vue
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-<template>
-  <div>
-    订单
-  </div>
-</template>
-
-<script type="text/ecmascript-6">
-  export default {
-  }
-</script>
-
-<style lang='stylus' rel='stylesheet/stylus' scoped>
-
- 
-</style>
-=======
 <template>
   <div>
     <router-view></router-view>
@@ -30,4 +13,3 @@
 
  
 </style>
->>>>>>> 5088e5e17ebfef8898ffa27abef6a3d5ea250f96
