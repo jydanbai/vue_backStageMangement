ab8e2485c62f281fa212c4892f962a31f4ba3c83
qiaoquan <1486314790@qq.com>
2019-11-24 16:08:41 +0800

    可以添加

--- src/pages/Orders/OrdersList/OrdersList.vue
+++ src/pages/Orders/OrdersList/OrdersList.vue
@@ -268,7 +268,7 @@
               <el-table-column label="操作">
                 <template slot-scope="scope">
                   <el-button size="mini">查看</el-button>
-                  <el-button size="mini" type="danger"  @click.native.prevent="deleteRow(scope.$index, tableData)"  >删除</el-button>
+                  <el-button size="mini" type="danger"  @click="handleDelete(scope.$index, scope.row)"  >删除</el-button>
                 </template>
               </el-table-column>
             </el-table>
@@ -343,7 +343,13 @@ export default {
     },
    guanbizhezhao(){
       this.shou1 = true;
-   }
+   },
+    handleDelete(index, row) {
+      console.log(index, row)
+        if(row.id == this.orderDatas.id ){
+           this.orderDatas.id = ''
+        }
+      }
   },
 
   //读取状态数据
